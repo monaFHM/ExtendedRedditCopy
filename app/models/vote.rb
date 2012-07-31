7590e852eba223ee7e9b99737e36075f8678c0b7
Steffen Schildknecht <steffen.schildknecht@zweitag.de>
2012-07-31 18:25:07 +0200

    Remove changes made during commenting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,5 +4,7 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  #COMMENT: The up_or_down field should be called up, 
+  #since the boolean indicates whether the vote is an upvote.
   validates :up_or_down, :user_id, :post_id, :presence => true
 end
